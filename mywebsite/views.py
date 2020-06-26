3c9145915e597227c1a89fe09c0e53208a91cf91
ndawinata <nanda.winata@gmail.com>
2020-06-27 03:30:20 +0700

    teamplate variabel dan tempalte teks

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # method view
 def index(request):
-    return render(request, 'index.html')
+    context={
+        'judul':'Halaman Home',
+        'penulis':'nanda winata',
+        'nav':[
+            ['/','Home'],
+            ['about/','About'],
+            ['blog/','Blog'],
+            ['blog/recent/','Recent']
+        ]
+    }
+    return render(request, 'index.html', context)
 
 def about(request):
     return HttpResponse('ini about')
