298d6b1faf942e3639fae3775507bd96f00b1d6f
ndawinata <nanda.winata@gmail.com>
2020-06-27 02:32:27 +0700

    start

---
+++ mywebsite/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# method view
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse('ini about')
