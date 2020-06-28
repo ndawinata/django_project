a4774aa1f2ba0226e4c809206191e5696aad57e9
ndawinata <nanda.winata@gmail.com>
2020-06-28 12:51:17 +0700

    berhasil menambahkan form dan mengeluarkan prediksi

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -9,6 +9,8 @@ def index(request):
     model = pickle.load(f)
     f.close()
     print(model.predict([[6,148,72,35,0,33.6,0.627,50]]))
+    
+    
 
     context = {
         'judul': 'Halaman Home',
@@ -19,8 +21,19 @@ def index(request):
             ['blog/', 'Blog'],
             ['blog/recent/', 'Recent']
         ],
-        'hasil': model.predict([[6,148,72,35,0,33.6,0.627,50]]),
     }
+    # print(request.method)
+    if request.method == 'POST':
+        preg = request.POST['pregnancies']
+        gluc= request.POST['glucose']
+        blood = request.POST['bloodPressure']
+        skin = request.POST['skin']
+        insul = request.POST['insulin']
+        bmi = request.POST['bmi']
+        diab = request.POST['diabetesPedigreeFunction']
+        age = request.POST['age']
+        context['hasil'] = model.predict([[preg,gluc,blood,skin,insul,bmi,diab,age]])
+
     return render(request, 'index.html', context)
 
 
